8d1dec9966dbcf44733959f8a11728c59920b5d9
tensorwarriormayank <ranjanmayank880@gmail.com>
2023-12-26 16:25:57 +0530

    ADD: added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./pages/Home/Home.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Shared/Footer/Footer.jsx";
 import { BrowserRouter, Routes , Route } from "react-router-dom";
 import Not_Found from "./pages/404/Not_found.jsx";
 
@@ -15,6 +16,7 @@ function App() {
        <Route path="/" element={<Home />} />
        <Route path="*" element={<Not_Found/>} />
       </Routes>
+      <Footer />
       {/* <div className="slider"></div> */}
     </BrowserRouter>
   );
---
+++ src/components/Shared/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <div className={styles.FooterDiv}>
+        <div>
+          <div>
+          {/* <FontAwesomeIcon icon="fa-solid fa-location-dot" style={{color: "#916969",}} /> */}
+          </div>
+        </div>
+        <div>
+            <div></div>
+        </div>
+        <div></div>
+    </div>
+  );
+};
+
+export default Footer;
