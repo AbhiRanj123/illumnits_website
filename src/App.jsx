b5b06c8ed4a15fb3f6ef85c4ffa0740596170250
AbhiRanj123 <abhiskek21_ug@cse.nits.ac.in>
2023-12-28 20:22:39 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
-// import Footer from "./components/Shared/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar.jsx";
-// import Contact from "./components/Contact/contact.jsx";
-import NotFound from "./pages/404/Not_found.jsx";
 import Contact from "./components/Contact/contact.jsx";
+import NotFound from "./pages/404/Not_found.jsx";
 import Footer from "./components/Shared/Footer/Footer.jsx";
 
 function App() {
@@ -16,7 +14,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/contact" element={<Contact/>}/>
       </Routes>
       <Footer />
       {/* <div className="slider"></div> */}
