64fd64ecbc3c2806febd95a92e444a599efe6e6d
ruler45 <sahin0945@gmail.com>
2023-12-28 20:19:15 +0530

    small bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import "./App.css";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 // import Footer from "./components/Shared/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar.jsx";
-// import Contact from "./components/Contact/contact.jsx";
+import Navbar from "./components/Shared/Navbar/Navbar.jsx";
 import NotFound from "./pages/404/Not_found.jsx";
 import Contact from "./components/Contact/contact.jsx";
 import Footer from "./components/Shared/Footer/Footer.jsx";
@@ -16,7 +15,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
-        <Route path="/contact" element={<Contact/>}/>
       </Routes>
       <Footer />
       {/* <div className="slider"></div> */}
