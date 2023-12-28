2b70403a8781f2b0c03db7a9b53b8f1c1e569b9c
AbhiRanj123 <abhiskek21_ug@cse.nits.ac.in>
2023-12-28 21:31:48 +0530

    minor changes

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
               <Icon width={40} icon="tabler:mail-filled" color="#785140" />
             </div>
             <div className={styles.content}>
-              <Link to="mailto:illuminits@gmail.com">illuminits@gmail.com</Link>
+              <Link to="mailto:illuminits@nits.ac.in">illuminits@nits.ac.in</Link>
             </div>
           </div>
           <div className={styles.info}>
