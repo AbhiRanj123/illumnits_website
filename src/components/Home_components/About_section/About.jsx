f2442d0e409db7376c6d37c5fca65a52edbc88b2
AbhiRanj123 <abhiskek21_ug@cse.nits.ac.in>
2024-01-18 02:59:03 +0530

    updation

--- src/components/Home_components/About_section/About.jsx
+++ src/components/Home_components/About_section/About.jsx
@@ -7,17 +7,14 @@ function About() {
         <h2 className="about-title">About ILLUMNITS</h2>
         <h3>Literary, Publication and Fine Arts Society of NIT Silchar</h3>
         <p>
-          Illuminits is the official Literary, Publication and Fine Arts Society of NIT
-          Silchar. It is a student body which aims to promote and nurture the literary and
+          ILLUMINITS is the official Literary, Publication and Fine Arts Society of NIT
+          Silchar. The Society always aims to promote and nurture the literary and
           artistic talents of the students of NIT Silchar. It is a platform for the
-          students to showcase their talents in the fields of literature, art, music,
-          dance, drama, photography, etc. Illuminites also publishes the annual magazine
-          of NIT Silchar, “The Alchemist”. The society also organizes various events
-          throughout the year, such as the annual literary fest, “The Alchemist”, the
-          annual photography contest, “The Shutterbug”, the annual art competition, “The
-          Canvas”, and many more. Illuminites also publishes the annual magazine of NIT
-          Silchar, “The Alchemist”.
-        </p>
+          students to showcase their talents in the fields of literature, art, public speaking etc.
+          IGNITS is the annual magazine of ILLLUMINITS where students pen down thier talents. 
+          The society also organizes various events throughout the year, such as the annual 
+          literary fest of NIT Silchar, “LPL”, varoius events during IMLD, Manthan etc.
+          </p>
 
         <h3>Mission of ILLUMNITS</h3>
         <p>
