6942f2b203e557d73a8739d5ecfc5c763f9c7539
Him_X_Raj <rajhimanshu4304@gmail.com>
2024-01-21 00:08:48 +0530

    club section updated

--- src/pages/Clubs Pages/ClubsPages.jsx
+++ src/pages/Clubs Pages/ClubsPages.jsx
@@ -37,75 +37,51 @@ const ClubsPages = () => {
       {
         clubDetails &&(
           <>
-          <div className="club-card">
-            <div className="club-content">
-              <p className="club-name">{clubName}</p>
-              <div className="details-info">
-                <p className="text-detail">{clubDetails.section1.paragraph1}</p>
+          <div className="club-card-0">
+              <p className="club-card-0-heading">{clubName}</p>
+              <div className="club-card-0-info">
+                <p className="club-card-0-text-detail">{clubDetails.description}</p>
               </div>
-            </div>
           </div>
 
-          <div className="image-text-1">
-            <div className="image-text-1-card">
-              <div className="card-left">
+          <div className="club-card-1">
+              <div className="club-card-1-left">
                 <img
                   className="image"
-                  src="https://th.bing.com/th/id/OIP.iAhcp6m_91O-ClK79h8EQQHaFj?rs=1&pid=ImgDetMain"
+                  src={clubDetails.section1.image}
                 />
               </div>
-              <div className="card-right">
-                <p className="card-right-heading">Lorem ipsum dolor sit amet, consec</p>
-                <p className="card-right-info">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat. Duis aute irure dolor in reprehenderit.Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat.
+              <div className="club-card-1-right">
+                <p className="club-card-1-right-heading">{clubDetails.section1.heading}</p>
+                <p className="club-card-1-right-info">
+                  {clubDetails.section1.paragraph}
                 </p>
               </div>
-            </div>
           </div>
 
-          <div className="image-text-2">
-            <div className="info-text-2-data">
-              <div className="info-left">
-                <p className="card-info-right">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat. Duis aute irure dolor in reprehenderit.Lorem ipsum
-                  dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                  incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis
+          <div className="club-card-2">
+              <div className="club-card-2-left">
+                <p className="club-card-2-left-info">
+                  {clubDetails.section2.paragraph}
                 </p>
               </div>
-              <div className="info-right">
+              <div className="club-card-2-right">
                 <img
-                  className="image-left"
-                  src="https://th.bing.com/th/id/OIP.iAhcp6m_91O-ClK79h8EQQHaFj?rs=1&pid=ImgDetMain"
+                  className="image"
+                  src={clubDetails.section2.image}
                 />
               </div>
-            </div>
           </div>
 
-          <div className="lorem-card">
-            <div className="lorem-card-component">
-              <div className="lorem-details">
-                <p className="lorem-heading">Lorem ipsum dolor sit amet</p>
-                <p className="lorem-text">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo.
-                </p>
-              </div>
-              <div className="lower-image">
-                <img src="https://th.bing.com/th/id/OIP.iAhcp6m_91O-ClK79h8EQQHaFj?rs=1&pid=ImgDetMain" />
-              </div>
+          <div className="club-card-3">
+            <div className="club-card-3-top">
+              <p className="club-card-3-top-heading">{clubDetails.section3.heading}</p>
+              <p className="club-card-3-top-info">
+                {clubDetails.section3.paragraph}
+              </p>
+            </div>
+            <div className="club-card-3-bottom">
+              <img src={clubDetails.section3.image} />
             </div>
           </div>
         </>
