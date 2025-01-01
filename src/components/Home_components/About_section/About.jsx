4a43e4763fd247177374fd501c9a731224e2b121
Priyadas732 <pd2820523@gmail.com>
2025-01-01 12:01:38 +0530

    changesDone

--- src/components/Home_components/About_section/About.jsx
+++ src/components/Home_components/About_section/About.jsx
@@ -7,18 +7,19 @@ function About() {
         <h2 className="about-title">About ILLUMNITS</h2>
         <h3>Literary, Publication and Fine Arts Society of NIT Silchar</h3>
         <p>
-          ILLUMINITS is the official Literary, Publication and Fine Arts Society of NIT
-          Silchar. The Society always aims to promote and nurture the literary and
-          artistic talents of the students of NIT Silchar. It is a platform for the
-          students to showcase their talents in the fields of literature, art, public speaking etc.
-          IGNITS is the annual magazine of ILLLUMINITS where students pen down thier talents. 
-          The society also organizes various events throughout the year, such as the annual 
-          literary fest of NIT Silchar, “LPL”, varoius events during IMLD, Manthan etc.
-          </p>
+          ILLUMINITS, the Literary, Publications, and Fine Arts Society at NIT Silchar
+          conducts a number of events under its yearly almanac. It presents great opportunity
+          for individuals who are disposed to express their creativity and share their unique
+          ideas and opinions. It provides a space for people to celebrate and appreciate their
+          artistic endeavours allowing storytellers to weave words together building dynamic
+          tales, poets to unlock the ability to convey emotions in their rawest form, speakers to
+          make a link between ideas and words and artists to infuse creativity, thought and
+          imagination into tangible forms.
+        </p>
 
         <h3>Mission of ILLUMNITS</h3>
         <p>
-        The mission of the ILLUMINITS, the Literary, Publications, and Fine Arts Society at NIT Silchar is to cultivate a thriving community that champions the fusion of literature, publications, and fine arts as powerful mediums for creative expression. The Society encompasses the three main pillars namely Literature, Publication and Fine Arts.Through these three interconnected pillars, our club seeks to foster a sense of creativity, curiosity, and cultural appreciation. We aspire to empower individuals to not only express themselves but also to connect with others who share a passion for the transformative power of literature and the arts. Together, we strive to create a legacy of artistic excellence and a community that celebrates the beauty of human expression.
+          The mission of the ILLUMINITS, the Literary, Publications, and Fine Arts Society at NIT Silchar is to cultivate a thriving community that champions the fusion of literature, publications, and fine arts as powerful mediums for creative expression. The Society encompasses the three main pillars namely Literature, Publication and Fine Arts.Through these three interconnected pillars, our club seeks to foster a sense of creativity, curiosity, and cultural appreciation. We aspire to empower individuals to not only express themselves but also to connect with others who share a passion for the transformative power of literature and the arts. Together, we strive to create a legacy of artistic excellence and a community that celebrates the beauty of human expression.
         </p>
       </div>
     </div>
